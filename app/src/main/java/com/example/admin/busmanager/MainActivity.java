a619a98e2cbe7ea506131fea193bf26ab73e2c6d
nguyenleanhkhoa <kyoshinichiadv@outlook.com>
2017-09-20 01:58:21 +0700

    thêm Toast

--- app/src/main/java/com/example/admin/busmanager/MainActivity.java
+++ app/src/main/java/com/example/admin/busmanager/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 
     @Override

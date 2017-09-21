b807f6ceda0ab19b763bfa0f6fde057fc204ab85
DESKTOP-F8CJVE5\BRUCE <brucebui5041@gmail.com>
2017-09-21 20:42:52 +0700

    đã thêm Toát

--- app/src/main/java/com/example/admin/busmanager/MainActivity.java
+++ app/src/main/java/com/example/admin/busmanager/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.admin.busmanager;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Toast.makeText(this, "Bui Hong Lan", Toast.LENGTH_SHORT).show();
 
     }
 
